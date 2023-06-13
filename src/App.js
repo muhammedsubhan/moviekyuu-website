a00e884e0789d362bf5838be0cd959887fb7e9e5
Subhan <muhammedsubhan692@gmail.com>
2023-06-13 05:14:17 +0500

    Header completed

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from "react";
-// import { Route, Routes } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/Navbar/Navabr";
+import Header from "./components/Header/Header";
 const App = () => {
   return (
     <>
       <div className="App">
         <Navbar />
+        <Routes>
+          <Route path="/" element={<Header />} />
+        </Routes>
       </div>
     </>
   );
