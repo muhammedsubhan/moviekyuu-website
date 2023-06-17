9d4ef088d9a095200042f1f05998fb985ffa483b
Subhan <muhammedsubhan692@gmail.com>
2023-06-17 23:54:49 +0500

    Movie s component

--- src/components/Navbar/Navabr.jsx
+++ src/components/Navbar/Navabr.jsx
@@ -5,31 +5,33 @@ import Search from "../search/Search";
 const Navabr = () => {
   return (
     <>
-      <div className="flex items-center justify-between nav-border mb-2 ">
-        <div className="ml-20 font-display hidden md:flex p-3">
-          <Link to="/" className="mr-6 flex items-center space-x-2">
-            <div className="flex flex-row flex-nowrap items-center gap-2 md:-ml-12">
-              <div className="bg-purple-600 h-8 w-8 rounded-full text-center items-center flex justify-center">
-                🎬
+      <div className="">
+        <div className="flex items-center justify-between nav-border mb-2">
+          <div className="ml-20 font-display hidden md:flex p-3 ">
+            <Link to="/" className="mr-6 flex items-center space-x-2">
+              <div className="flex flex-row flex-nowrap items-center gap-2 md:-ml-12">
+                <div className="bg-purple-600 h-8 w-8 rounded-full text-center items-center flex justify-center">
+                  🎬
+                </div>
+                <span className="font-bold sm:inline-block text-white">
+                  MovieKyuu
+                </span>
               </div>
-              <span className="font-bold sm:inline-block text-white">
-                MovieKyuu
-              </span>
-            </div>
-          </Link>
-          <nav className="flex items-center space-x-6 text-sm font-medium">
-            <Link to="/movie" className=" text-white hover:text-gray-300">
-              Movies
             </Link>
-            <Link to="/my-list" className=" text-white hover:text-gray-300">
-              My List
-            </Link>
-            <Link to="/about" className=" text-white hover:text-gray-300">
-              About
-            </Link>
-          </nav>
+            <nav className="flex items-center space-x-6 text-sm font-medium">
+              <Link to="/movie" className=" text-white hover:text-gray-300">
+                Movies
+              </Link>
+              <Link to="/my-list" className=" text-white hover:text-gray-300">
+                My List
+              </Link>
+              <Link to="/about" className=" text-white hover:text-gray-300">
+                About
+              </Link>
+            </nav>
+          </div>
+          <Search />
         </div>
-        <Search />
       </div>
     </>
   );
